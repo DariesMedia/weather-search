abe80aa6f70bb846d031b32cb9dec9cb0e2a4b52
ABAI DANIEL ADEWUNMI <51218816+DariesMedia@users.noreply.github.com>
2021-02-09 17:43:04 +0100

    Update locate.js

--- locate.js
+++ locate.js
@@ -5,72 +5,67 @@ $(function(){
 
   
   // Search location Function
-  $( "#go" ).click(function(){
+  $("#search").keyup(function (event) {  
+    if(event.keyCode == 13) { // 13 = Enter Key
+      var city = $("input").val().toUpperCase(); // Input from the user
+      if (city == " "){
+        
+      }else{
+        // Geocode the search string
+        var searchMe = "'"+"https://www.mapquestapi.com/staticmap/v5/map?key=G4Gpr5zmanNAyWUIii9LMavfNPVbdIy8&scalebar=true|bottom&locations="+city+"&zoom=14&type=hyb&defaultMarker=marker-sm-red-green-G&size=600,200@2x&declutter=false"+"'";
 
-    var city = $("input").val(); // Input from the user
+        // Getting a static map image by embeding into mapquest api query
+        $(".map").html('<img src='+ searchMe+'  />');
 
-    if (city == ""){
-      alert("Invalid Name of Place");
-    }else{
-      // Geocode the search string
-      var searchMe = "'"+"https://www.mapquestapi.com/staticmap/v5/map?key=G4Gpr5zmanNAyWUIii9LMavfNPVbdIy8&scalebar=true|bottom&locations="+city+"&zoom=14&type=hyb&defaultMarker=marker-sm-red-green-G&size=600,400@2x&declutter=false"+"'";
+        // Getting Weather Parameters from api
+        var jqxhr = $.getJSON( "http://api.openweathermap.org/data/2.5/weather?q="+city+"&APPID=b9f0751281511bff6a95df69b37a95bd", function(data) {
 
-      // Getting a static map image by embeding into mapquest api query
-      $(".map").html('"<img src='+ searchMe+'  />"');
+          // Varibles for all weather conditions
+          var weatherCondition = data.weather[0].description;
+          var temperature = data.main.temp;
+          var pressure = data.main.pressure;
+          var humidity = data.main.humidity;
+          var windSpeed = data.wind.speed;
+          var windDeg = data.wind.deg;
+          var celsius = temperature - 273.15;
+          celsius = Math.floor(celsius);
+          var fahrenheit = (celsius * 9/5) + 32;
+          fahrenheit = Math.floor(fahrenheit);
 
-      // Getting Weather Parameters from api
-      var jqxhr = $.getJSON( "http://api.openweathermap.org/data/2.5/weather?q="+city+"&APPID=b9f0751281511bff6a95df69b37a95bd", function(data) {
-
-        // Varibles for all weather conditions
-        var weatherCondition = data.weather[0].description;
-        var temperature = data.main.temp;
-        var pressure = data.main.pressure;
-        var humidity = data.main.humidity;
-        var windSpeed = data.wind.speed;
-        var windDeg = data.wind.deg;
-        var celsius = temperature - 273.15;
-        var fahrenheit = (celsius * 9/5) + 32;
-
-        // Adding weather variables in html 
-        $("#interest").text("Place of Interest: "+city);
-        $("#weatherC").text("The weather condition is "+ weatherCondition);
-        $("#temperature").text("Temperature: "+ temperature+"K");
-        $("#pressure").text("Pressure: "+ pressure+"hPa");
-        $("#windspeed").text("Wind speed: "+ windSpeed +"meter/sec"+ " at " + windDeg+"deg");
-        $("#humidity").text("Humidity: "+ humidity+"%");
-        // Function Converting celsius to fahrenheit
-        $("#fahrenheit").click(function(){
-          $(this).text("Temperature: "+ fahrenheit+"F");
-          $(this).css({
-            backgroundColor: "blue",
+          // Adding weather variables in html 
+          $("#interest").text(city + " WEATHER");
+          $("#weatherC").text(weatherCondition);
+          $("#temperature").text(temperature+"°K");
+          $("#pressure").text(pressure+"hPa" + " Pressure");
+          $("#windspeed").text(windSpeed +"meter/sec"+ " at " + windDeg+"deg" + " Wind Speed");
+          $("#humidity").text(humidity+"%" + " Humidity");
+          // Function Converting celsius to fahrenheit
+          $("#fahrenheit").click(function(){
+            $(this).text("Temperature: "+ fahrenheit+"°F");
+            $(this).css({
+              backgroundColor: "#0d1137",
+              padding: "10px;",
+              borderRadius: "10px 10px",
+              color: "white",
+            });
           });
-        });
-        // Function Converting temperature to celsius
-        $("#celsius").click(function(){
-          $(this).text("Temperature: "+ celsius+"C");
-          $(this).css({
-            backgroundColor: "blue",
+          // Function Converting temperature to celsius
+          $("#celsius").click(function(){
+            $(this).text("Temperature: "+ celsius+"°C");
+            $(this).css({
+              backgroundColor: "#0d1137",
+              padding: "10px;",
+              borderRadius: "10px 10px",
+              color: "white",
+            });
           });
-        });
-      }).done(function() {
-          console.log("Success");
-        }); 
-      // End of Weather api request
-      $(".more_stats").show();
+        }).done(function() {
+            console.log("Success");
+          }); 
+        // End of Weather api request
+        $(".more_stats").show();
+      }
     }
   });  
   // End of search location function 
-
-  $("header nav ul li:nth-child(2) a").click(function(){
-    $("html, body").delay(500).animate({
-      scrollTop: $("#resultsC").offset().top 
-  }, 500);
-  });
-
-  $("header nav ul li:nth-child(4) a").click(function(){
-    $("html, body").delay(500).animate({
-      scrollTop: $("#aboutus").offset().top
-  }, 500);
-  });
-
 });
